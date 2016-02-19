2dd7efbc4d1c8f0b0ff5492be86104f1a811bced
Gulshan Gill <gulshngill@Gulshans-MacBook-Pro.local>
2016-02-19 17:37:47 +0800

    Added dependencies

--- FlickrBrowser/app/src/main/java/com/example/gulshngill/flickrbrowser/GetRawData.java
+++ FlickrBrowser/app/src/main/java/com/example/gulshngill/flickrbrowser/GetRawData.java
@@ -1,18 +1,109 @@
 package com.example.gulshngill.flickrbrowser;
 
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import android.util.Log;
+
 /**
  * Created by gulshngill on 15/02/2016.
  */
-enum DownloadStaus {IDLE, PROCESSING, NOT_INITIALISED,FAILED_OR_EMPTY, OK }
+enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALISED,FAILED_OR_EMPTY, OK }
 
 
 public class GetRawData {
     private String LOG_TAG = GetRawData.class.getSimpleName();
     private String mRawUrl;
     private String mData;
-    private DownloadStaus mDownloadStatus;
+    private DownloadStatus mDownloadStatus;
 
     public GetRawData(String mRawUrl) {
         this.mRawUrl = mRawUrl;
+        this.mDownloadStatus = DownloadStatus.IDLE;
     }
+
+    public void reset() {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        this.mRawUrl = null;
+        this.mData = null;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public class DownloadRawData extends AsyncTask<String, Void, String> {
+        protected void onPostExecute(String webData) {
+            mData = webData;
+            Log.v(LOG_TAG, "Returned Data" + mData);
+
+            if(mData == null) {
+                if(mRawUrl == null) {
+                    mDownloadStatus = DownloadStatus.NOT_INITIALISED;
+                } else {
+                    mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+                }
+            } else {
+                mDownloadStatus = DownloadStatus.OK; //Success
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            if(params == null)
+                return null;
+
+            try {
+                URL url = new URL(params[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                if(inputStream == null)
+                    return null;
+
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                return buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error", e);
+                return null;
+            } finally {
+                if(urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if(reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+        }
+    }
+
 }
