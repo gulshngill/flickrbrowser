8c2ff389ad9cbdcd0e67804fac008b0b2c0ce336
gulshngill <gulshn.gill@gmail.com>
2016-04-18 18:19:15 +0800

    Completed basic gui

--- FlickrBrowser/app/src/main/java/com/example/gulshngill/flickrbrowser/MainActivity.java
+++ FlickrBrowser/app/src/main/java/com/example/gulshngill/flickrbrowser/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,20 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        GetFlickrJsonData jsonData = new GetFlickrJsonData("android, lollipop", true);
-        jsonData.execute();
+        ProcessPhoto processPhoto = new ProcessPhoto("starcraft", true);
+        processPhoto.execute();
     }
 
     @Override
@@ -68,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void execute(){
-            super.execute();
+            //super.execute();
             ProcessData processData = new ProcessData();
             processData.execute();
         }
